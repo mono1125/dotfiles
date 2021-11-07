c20d8fa8d537055376c7ce298eb36a2f83692ae5
F <example@example.com>
2021-11-07 17:52:17 +0900

    mod lsp.sh

--- lsp.sh
+++ lsp.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+if !(type "npm" > /dev/null 2>&1); then
+    if (type "apt" > /dev/null 2>&1); then
+        sudo apt install nodejs npm
+    elif (type "yum" > /dev/null 2>&1); then
+        sudo yum install nodejs npm
+    else
+        echo "Not found apt and yum"
+    fi
+fi
+
 npm install -g yarn
 npm install -g bash-language-server
 pip install jedi-language-server
