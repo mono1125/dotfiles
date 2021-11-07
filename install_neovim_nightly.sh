4ed94387c3f33bb44a670e67481862fea9fd68b9
F <example@example.com>
2021-11-07 17:40:00 +0900

    mod install_neovim_nightly

--- install_neovim_nightly.sh
+++ install_neovim_nightly.sh
@@ -3,3 +3,6 @@
 curl -L https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage -o /tmp/nvim-nightly
 chmod +x /tmp/nvim-nightly
 sudo mv /tmp/nvim-nightly /usr/local/bin
+echo "Installed /usr/local/bin/nvim-nightly"
+echo "add shell config:"
+echo 'alias nvim="/usr/local/bin/nvim-nightly"'
