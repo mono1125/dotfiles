963d208d28a81544bb912cdfd232fbef974bdfa0
mono <mono@venus.local>
2022-10-22 20:26:05 +0900

    nvim add lspconfig, null-ls: rust

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -19,6 +19,7 @@ null_ls.setup {
     null_ls.builtins.formatting.prettierd,
     null_ls.builtins.formatting.black.with { extra_args = { "--fast" } },
     null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.rustfmt,
     null_ls.builtins.diagnostics.eslint_d.with({
       diagnostics_format = '[eslint] #{m}\n(#{c})'
     }),
